d216199831a9535ca9178a22ec642f7c7f623424
hanamorsy <hana0morsy@gmail.com>
2018-04-24 03:30:31 +0200

    Time + Score + Menu
    create time for the game and score of the player and menu that hold tools that help the player

---
+++ Student From Hell new/Assets/scripts/timescript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+#pragma warning disable 0649
+public class timescript : MonoBehaviour {
+	public Image timerBar=null;
+	public float maxTime=20;
+	public float timeLeft;
+	//public GameObject TimesUpText;
+	// Use this for initialization
+	void Start () {
+		//TimesUpText.SetActive (false);
+		timerBar.GetComponent<Image> ();
+		timeLeft = maxTime;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (timeLeft > 0) {
+			timeLeft -= Time.deltaTime;
+			timerBar.fillAmount = timeLeft / maxTime;
+		} else {
+			//TimesUpText.SetActive (true);
+			Time.timeScale = 0;
+		}
+	}
+}
