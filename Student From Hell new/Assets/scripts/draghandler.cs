a66bf8ef24b574953d25ab46c2b0ba74bd14fdfd
hanamorsy <hana0morsy@gmail.com>
2018-04-27 01:04:27 +0200

    Drag Handler for The Menu

---
+++ Student From Hell new/Assets/scripts/draghandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class draghandler : MonoBehaviour ,IDragHandler,IEndDragHandler {
+
+	// Use this for initialization
+	public void OnDrag(PointerEventData AxisEventData){
+		transform.position = Input.mousePosition;
+	}
+	public void OnEndDrag(PointerEventData AxisEventData){
+		transform.localPosition = Vector3.zero;
+	}
+	void Start () {
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
